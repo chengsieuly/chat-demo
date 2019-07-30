34a6bd0f7572c84247d60a3e1d7e65e24008d048
Cheng Ly <chengsieuly@gmail.com>
2019-07-30 12:48:11 -0700

    aws setup

--- index.js
+++ index.js
@@ -4,41 +4,41 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = 3000;
+var port = PORT || 3000;
 
 server.listen(port, ip.address(), () => {
-  console.log('Server listening at http://%s:%d', ip.address(), port);
+    console.log('Server listening at http://%s:%d', ip.address(), port);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-  var addedUser = false;
+    var addedUser = false;
 
-  socket.on('new message', (data) => {
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
+    socket.on('new message', (data) => {
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
     });
-  });
 
-  socket.on('add user', (username) => {
-    if (addedUser) return;
+    socket.on('add user', (username) => {
+        if (addedUser) return;
 
-    socket.username = username;
-    addedUser = true;
-    socket.emit('login');
+        socket.username = username;
+        addedUser = true;
+        socket.emit('login');
 
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+        });
     });
-  });
 
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-      });
-    }
-  });
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+            });
+        }
+    });
 });
---
+++ vocabulary.md
@@ -0,0 +1,22 @@
+## HTML
+### css selectors
+Targets specific elements on the page
+
+### class
+Provides a shareable identifier across elements
+
+### css block
+Block of styles targetting a selector
+
+## JavaScript
+### variable
+A container for values
+
+### function
+A set of instuctions that performs a certain action
+
+### method
+A function within a library
+
+### library
+A set of methods that performs varying actions
